75854ecb43cc489e4d4b44a84961574ad1e54801
rohan <rohansharmas028@gmail.com>
2025-02-28 23:55:38 +0530

    created and updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import './index.css'; // Ensure this import exists
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
